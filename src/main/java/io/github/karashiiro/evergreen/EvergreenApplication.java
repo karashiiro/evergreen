5ef4b2f412275db76f3b6ea2bece68148cd38539
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-07-02 17:13:42 -0700

    Add project files

---
+++ src/main/java/io/github/karashiiro/evergreen/EvergreenApplication.java
@@ -0,0 +1,13 @@
+package io.github.karashiiro.evergreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvergreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvergreenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/karashiiro/evergreen/EvergreenApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.karashiiro.evergreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvergreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
